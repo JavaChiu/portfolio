9fe56cf6db400562631e223e8278a6eb0fb624af
chungan <chungan@uchicago.edu>
2017-12-12 20:01:29 -0600

    adding styles

--- index.php
+++ index.php
@@ -1,4 +1,16 @@
-<link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Lobster" />
+<!DOCTYPE html>
+<html>
+<head>
+<title>Chung An Chiu</title>
+<link rel="stylesheet" type="text/css" href="./css/styles.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lobster">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css" />
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-social/5.1.1/bootstrap-social.css" />
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" />
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-beta/js/bootstrap.min.js"></script>
+</head>
+<body>
 
 <nav class="navbar navbar-default">
     <div class="container-fluid">
@@ -102,3 +114,6 @@
         </ul>
     </div>
 </footer>
+
+</body>
+</html>
