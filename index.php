a20b041436251e7cd966513f0b7a56320eabf1e7
chungan <chungan@uchicago.edu>
2017-12-12 20:55:03 -0600

    add index.js

--- index.php
+++ index.php
@@ -10,6 +10,7 @@
     <link rel="stylesheet" type="text/css" href="./css/styles.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-beta/js/bootstrap.min.js"></script>
+    <script src="./js/index.js"></script>
 </head>
 
 <body>
