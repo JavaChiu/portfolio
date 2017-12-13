629e00096f6bb66d3cd3799277dc32d3a54c1213
chungan <chungan@uchicago.edu>
2017-12-12 20:57:02 -0600

    revise index.js

--- js/index.js
+++ js/index.js
@@ -1,14 +1,7 @@
-// Highlight the top nav as scrolling occurs
-$('body').scrollspy({target: '.navbar-fixed-top'})
+$(document).on('click', 'a[href^="#"]', function (event) {
+    event.preventDefault();
 
-// Closes the Responsive Menu on Menu Item Click
-$('.navbar-collapse ul li a').click(function() {
-  $(".navbar-collapse").collapse('hide');});
-
-// jQuery for page scrolling feature - requires jQuery Easing plugin
-$(function() {$('body').on('click', 'a.scrollable', function(event) {
-  var $anchor = $(this);
-  $('html, body').stop().animate({scrollTop: $($anchor.attr('href')).offset().top},1500,'easeInOutExpo');
-  event.preventDefault();
-  });
+    $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 500);
 });
