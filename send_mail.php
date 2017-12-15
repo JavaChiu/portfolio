10065729254a4c186e4811a8382ecd13867b9d65
chungan <chungan@uchicago.edu>
2017-12-15 11:56:31 -0600

    try mail

--- send_mail.php
+++ send_mail.php
@@ -1,4 +1,5 @@
 <?php
+/*
 $to      = 'chungan@uchicago.edu';
 $email = $_POST['email'];
 $subject = $_POST['title'];
@@ -8,4 +9,13 @@ $headers = 'From: ' . $email . "\r\n" .
     'X-Mailer: PHP/' . phpversion();
 
 mail($to, $subject, $message, $headers);
+*/
+
+$to = "chungan@uchicago.edu";
+$subject = "My subject";
+$txt = "Hello world!";
+$headers = "From: webmaster@example.com" . "\r\n" .
+"CC: somebodyelse@example.com";
+
+mail($to,$subject,$txt,$headers);
 ?> 
