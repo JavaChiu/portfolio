faff6b8f3a105353a6370ac622a6d1278dfe3b67
chungan <chungan@uchicago.edu>
2017-12-15 18:10:46 -0600

    add real info

--- send_mail.php
+++ send_mail.php
@@ -1,10 +1,26 @@
 <?php
+
+/*
+$to      = 'chungan@uchicago.edu';
+$email = $_POST['email'];
+$subject = $_POST['title'];
+$message = $_POST['message'];
+$headers = 'From: ' . $email . "\r\n" .
+    'Reply-To: chungan@uchicago.edu' . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+
+mail($to, $subject, $message, $headers);
+*/
+
+$email = $_POST['email'];
+$subject = $_POST['title'];
+$message = $_POST['message'];
+
 require 'vendor/autoload.php';
 
-$from = new SendGrid\Email(null, "test@example.com");
-$subject = "Hello World from the SendGrid PHP Library!";
+$from = new SendGrid\Email(null, $email);
 $to = new SendGrid\Email(null, "chungan@uchicago.edu");
-$content = new SendGrid\Content("text/plain", "Hello, Email!");
+$content = new SendGrid\Content("text/plain", $message);
 $mail = new SendGrid\Mail($from, $subject, $to, $content);
 
 $apiKey = getenv('SENDGRID_API_KEY');
@@ -12,6 +28,8 @@ $sg = new \SendGrid($apiKey);
 
 $response = $sg->client->mail()->send()->post($mail);
 echo $response->statusCode();
+echo '<br>';
 echo $response->headers();
+echo '<br>';
 echo $response->body();
 ?> 
