b9557ac6dbf8187a934a1bf3a15ccdf72ae3d0c4
chungan <chungan@uchicago.edu>
2017-12-15 19:28:20 -0600

    add name tag

--- send_mail.php
+++ send_mail.php
@@ -1,26 +1,15 @@
 <?php
 
-/*
-$to      = 'chungan@uchicago.edu';
-$email = $_POST['email'];
-$subject = $_POST['title'];
-$message = $_POST['message'];
-$headers = 'From: ' . $email . "\r\n" .
-    'Reply-To: chungan@uchicago.edu' . "\r\n" .
-    'X-Mailer: PHP/' . phpversion();
-
-mail($to, $subject, $message, $headers);
-*/
-
 require 'vendor/autoload.php';
 
 $email = $_POST['email'];
 $subject = $_POST['title'];
 $message = $_POST['message'];
 $name = $_POST['first-name']." ".$_POST['last-name'];
-print_r($_GET);
 
 /*
+print_r($_GET);
+
 echo $email;
 echo $subject;
 echo $message;
@@ -46,15 +35,17 @@ echo $content;
 echo "<br>";
 echo $subject;
 echo "<br>";
+*/
 
 $apiKey = getenv('SENDGRID_API_KEY');
 $sg = new \SendGrid($apiKey);
 
 $response = $sg->client->mail()->send()->post($mail);
+
 echo $response->statusCode();
 echo '<br>';
 echo $response->headers();
 echo '<br>';
 echo $response->body();
-*/
+
 ?> 
