bd526f0100b936678bfb64bbfb07bafdf7a4bac4
chungan <chungan@uchicago.edu>
2018-01-01 21:20:57 -0600

    add weather page

---
+++ js/weather.js
@@ -0,0 +1,41 @@
+function getWeather() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(requestWeather);
+    } else {
+        document.getElementById("city").innerHTML = "Geolocation is not supported by this browser.";
+    }
+}
+
+function requestWeather(position) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            //this.responseText;
+            var weather = JSON.parse(this.responseText);
+            document.getElementById("city").innerHTML = weather.name + ", " + weather.sys.country;
+            document.getElementById("description").innerHTML = weather.weather[0].main;
+            document.getElementById("degree").innerHTML = (weather.main.temp - 273.15).toFixed(1) + String.fromCharCode(176) + "C";
+            document.getElementById("icon").src = "https://openweathermap.org/img/w/" + weather.weather[0].icon + ".png";
+            //document.getElementById("citation").innerHTML = weather[0].title;
+        }
+    };
+    var url = "https://api.openweathermap.org/data/2.5/weather?lat=" + position.coords.latitude + "&lon=" + position.coords.longitude + "&appid=2c036e5e900438c4e810fc0eeb177602";
+    xhttp.open("GET", url, true);
+    xhttp.send();
+}
+
+function convertDegree() {
+    var current = document.getElementById("degree").innerHTML;
+    var len = current.length;
+    var newNum;
+    if (current.endsWith("C")) {
+        current = current.substring(0, len - 2);
+        newNum = (current * 9 / 5) + 32;
+        document.getElementById("degree").innerHTML = newNum.toFixed(1) + String.fromCharCode(176) + "F";
+    } else if (current.endsWith("F")) {
+        current = current.substring(0, len - 2);
+        newNum = (current - 32) * 5 / 9;
+        document.getElementById("degree").innerHTML = newNum.toFixed(1) + String.fromCharCode(176) + "C";
+    }
+
+}
---
+++ weather.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Chung An Chiu</title>
+    <link rel="icon" type="image/png" sizes="16x16" href="./img/favicon-16x16.png">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lobster">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-beta.2/css/bootstrap.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-social/5.1.1/bootstrap-social.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" />
+    <link rel="stylesheet" type="text/css" href="./css/weather.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-beta/js/bootstrap.min.js"></script>
+    <script src="./js/weather.js"></script>
+</head>
+
+<body onload="getWeather()">
+    <div id="main" class="container-fluid">
+        <h1 class="text-center">Local Weather App</h1>
+        <h2 id="city" class="text-center"></h2>
+        <div id="content" class="row">
+            <div class="col-md-4 col-sm-4 card text-center">
+                <div class="card-body">
+                    <!--<h4 class="card-title">Special title treatment</h4>-->
+                    <p id="description" class="card-text"></p>
+                </div>
+            </div>
+            <div class="col-md-4 col-sm-4 card text-center">
+                <div class="card-body">
+                    <p id="degree" class="card-text"></p>
+                    <button type="button" onclick="convertDegree()" class="btn btn-outline-secondary btn-sm">Convert</button>
+                </div>
+            </div>
+            <div class="col-md-4 col-sm-4 card">
+                <div class="card-body text-center">
+                    <img id="icon" src="" alt="weather icon">
+                </div>
+            </div>
+        </div>
+        <div class="text-center">
+        </div>
+
+    </div>
+
+    <footer id="footer" class="container-fluid text-center">
+        <!--<hr>-->
+        <p> Created by <a href="#">Chung-An Chiu</a></p>
+        <div>
+            <ul>
+                <li>
+                    <a class="btn btn-social-icon btn-linkedin" href="https://www.linkedin.com/in/chung-an-chiu-62b030147/" target="_blank">
+                        <span class="fa fa-linkedin"></span>
+                    </a>
+                </li>
+                <li>
+                    <a class="btn btn-social-icon btn-github" href="https://github.com/JavaChiu" target="_blank">
+                        <span class="fa fa-github"></span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </footer>
+</body>
