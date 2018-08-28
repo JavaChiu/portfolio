f2cecd3545ed39f34650346bfb0d0ccd71442f98
Andrew Chiu <bb5868@gmail.com>
2018-08-28 15:15:09 -0500

    position

--- index.php
+++ index.php
@@ -72,18 +72,6 @@
         <h2><strong>Portfolio</strong></h2>
         <hr>
         <div class="col-sm-6 col-xs-12">
-            <a href="https://itunes.apple.com/us/app/genmo/id1347965515?mt=8">
-                <img src="./img/genmo.png" class="img-responsive genmo" alt="Image">
-            </a>
-            <p>Project Genmo</p>
-        </div>
-        <div class="col-sm-6 col-xs-12">
-            <a href="https://peaceful-fortress-80870.herokuapp.com/swagger-ui.html#/">
-                <img src="./img/swagger-api.png" class="img-responsive" alt="Image">
-            </a>
-            <p>Java Spring-boot rest api</p>
-        </div>
-        <div class="col-sm-6 col-xs-12">
             <a href="https://powerful-atoll-43424.herokuapp.com/weather.php">
                 <img src="./img/weather_snapshot.png" class="img-responsive" alt="Local Weather App">
             </a>
@@ -94,6 +82,18 @@
                 <img src="./img/tweet_snapshot.png" class="img-responsive" alt="Random Quote Machine">
             </a>
             <p>Random Quote Machine</p>
+        </div>
+        <div class="col-sm-6 col-xs-12">
+            <a href="https://peaceful-fortress-80870.herokuapp.com/swagger-ui.html#/">
+                <img src="./img/swagger-api.png" class="img-responsive" alt="Image">
+            </a>
+            <p>Java Spring-boot rest api</p>
+        </div>
+        <div class="col-sm-6 col-xs-12">
+            <a href="https://itunes.apple.com/us/app/genmo/id1347965515?mt=8">
+                <img src="./img/genmo.png" class="img-responsive genmo" alt="Image">
+            </a>
+            <p>Project Genmo</p>
         </div>
     </div>
 
